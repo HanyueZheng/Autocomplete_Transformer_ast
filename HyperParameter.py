64e25c4115ead85f47d24186d5816a6cb2d1124c
RongTouchTouch <xuxiangrong1201@163.com>
2019-09-11 21:44:39 +0800

    kg_modified

--- HyperParameter.py
+++ HyperParameter.py
@@ -1,11 +1,11 @@
 chunk_len=100   #每次训练用多少个字符
 batch = 2        #每批几个输入输出
-nbatches = 3       #每轮训练几批
-epoch_number=1001 #要训练多少轮
+nbatches = 30       #每轮训练几批
+epoch_number=1000 #要训练多少轮
 epoches_of_loss_record=100 #每多少轮将损失输出到文本
-epoches_of_model_save=100   #每多少论存储一次模型
+epoches_of_model_save=1000   #每多少论存储一次模型
 transformer_size=6  #encoder和decoder有多少层
-predict_length=10   #预测结果的长度（字符数）
+predict_length=1   #预测结果的长度（字符数）
 beam_search_number=3 #束搜索的大小
 
 
--- run_kg.py
+++ run_kg.py
@@ -6,52 +6,17 @@ from torch.autograd import Variable
 from Batch import Batch, Batch_kg
 from Optim import NoamOpt, LabelSmoothing
 from Model import make_model, make_model_kg
-from Train import run_epoch, greedy_decode, beam_search_decode, SimpleLossCompute, run_epoch_kg
+from Train import run_epoch, greedy_decode, beam_search_decode, SimpleLossCompute, run_epoch_kg, beam_search_decode_kg
 from Dataloader import DataLoader_char, DataLoader_token, DataLoader_token_kg
 from HyperParameter import chunk_len, batch, nbatches, transformer_size, epoch_number, epoches_of_loss_record, \
-	predict_length
-# import numpy as np
-# from torch import nn
-# import torchsnooper
-# from torchtext import data, datasets
-# import spacy
+    predict_length
+
 
 seaborn.set_context(context="talk")
 
-#cre文本匹配
-#输入数据处理
-def data_gen_char(dataloader, batch, nbatches):
-    "为src-tgt复制任务生成随机数据"
-    for i in range(nbatches):
-        data_src = dataloader.next_chunk()[0].unsqueeze(0)
-        data_tgt = dataloader.next_chunk()[1].unsqueeze(0)
-        for j in range(batch - 1):
-            data_src = torch.cat([data_src, dataloader.next_chunk()[0].unsqueeze(0)], 0)
-            data_tgt = torch.cat([data_tgt, dataloader.next_chunk()[1].unsqueeze(0)], 0)
 
-        src = Variable(data_src, requires_grad=False)
-        tgt = Variable(data_tgt, requires_grad=False)
-        yield Batch(src, tgt, 0)
-
-
-# 共有nbatches*batch*(chunklen-1)条数据
-def data_gen_token(dataloader, batch, nbatches, chunk_len, device):
-    "为src-tgt复制任务生成随机数据"
-    for i in range(nbatches):
-        data_src = torch.empty(1, chunk_len - 1).long().to(device)
-        data_tgt = torch.empty(1, 2).long().to(device)
-        for k in range(batch):
-            src_tgt_pair = dataloader.next_chunk()
-            for j in range(0, len(src_tgt_pair)):
-                data_src = torch.cat([data_src, src_tgt_pair[j][0].unsqueeze(0)])
-                data_tgt = torch.cat([data_tgt, src_tgt_pair[j][1].unsqueeze(0)])
-            data_src = data_src[1:]
-            data_tgt = data_tgt[1:]
-        src = Variable(data_src, requires_grad=False)
-        tgt = Variable(data_tgt, requires_grad=False)
-        yield Batch(src, tgt, -1)
-
-
+# cre文本匹配
+# 输入数据处理
 # 共有nbatches*batch*(chunklen-1)条数据
 def data_gen_token_kg(dataloader, batch, nbatches, chunk_len, device):
     "为src-tgt复制任务生成随机数据"
@@ -78,10 +43,11 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    sys.argv.append('train')
+    # sys.argv.append('train')
+    sys.argv.append('inference')
     sys.argv.append('EN-ATP-V226.txt')
     sys.argv.append('token')
-    sys.argv.append('transformer3000.model')
+    sys.argv.append('transformer200.model')
     sys.argv.append('The ATP software is the core')
 
     if (len(sys.argv) < 2):
@@ -91,6 +57,14 @@ if __name__ == "__main__":
         sys.exit(0)
 
     method = sys.argv[1]
+
+    ents = []
+    with open("kg_embed/entity2id.txt") as fin:
+        fin.readline()
+        for line in fin:
+            name, id = line.strip().split("\t")
+            ents.append(name)
+
     if (method == "train"):
         filename = sys.argv[2]
         is_char_level = sys.argv[3] == 'char'
@@ -135,12 +109,6 @@ if __name__ == "__main__":
                           SimpleLossCompute(model.generator, criterion, None), nbatches, epoch)
 
         else:
-            ents = []
-            with open("kg_embed/entity2id.txt") as fin:
-                fin.readline()
-                for line in fin:
-                    name, id = line.strip().split("\t")
-                    ents.append(name)
 
             dataloader = DataLoader_token_kg(filename, ents, chunk_len, device)
             V = dataloader.vocabularyLoader.n_tokens  # vocabolary size
@@ -165,90 +133,51 @@ if __name__ == "__main__":
                 run_epoch_kg("test ", data_gen_token_kg(dataloader, batch, nbatches, chunk_len, device), model,
                           SimpleLossCompute(model.generator, criterion, None), nbatches, epoch)
 
+    elif method == "inference":
+        filename = sys.argv[2]
+        is_char_level = sys.argv[3] == 'char'
+        trained_model_name = sys.argv[4]
+        words = sys.argv[5]
 
+        if is_char_level:
+            model = torch.load(trained_model_name).cuda()
+            model.eval()
+            dataloader = DataLoader_char(filename, chunk_len, device)
+            src = Variable(dataloader.vocabularyLoader.char_tensor(words).unsqueeze(0))
+            src_mask = Variable((src != 0).unsqueeze(-2))
+            output_embed = greedy_decode(model, src, src_mask, max_len=predict_length)[0].cpu().numpy()
+            result = ""
+            for i in output_embed:
+                result += dataloader.vocabularyLoader.index2char[i]
+            print(result[1:])
 
-# if True:
-#     spacy_en = spacy.load('en_core_web_sm')
-#     spacy_de = spacy.load('de_core_news_sm')
-#     print("good")
-#
-#     def tokenize_de(text):
-#         return [tok.text for tok in spacy_de.tokenizer(text)]
-#
-#     def tokenize_en(text):
-#         return [tok.text for tok in spacy_en.tokenizer(text)]
-#
-#     BOS_WORD = '<s>'
-#     EOS_WORD = '</s>'
-#     BLANK_WORD = '<blank>'
-#     SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
-#     TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD,
-#                      eos_token = EOS_WORD, pad_token=BLANK_WORD)
-#
-#     MAX_LEN = 100
-#     train, val, test = datasets.IWSLT.splits(
-#         exts=('.de', '.en'), fields=(SRC, TGT),
-#         filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
-#             len(vars(x)['trg']) <= MAX_LEN)
-#     MIN_FREQ = 2
-#     SRC.build_vocab(train.src, min_freq=MIN_FREQ)
-#     TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
-#
-#
-# class MyIterator(data.Iterator):
-#     def create_batches(self):
-#         if self.train:
-#             def pool(d, random_shuffler):
-#                 for p in data.batch(d, self.batch_size * 100):
-#                     p_batch = data.batch(
-#                         sorted(p, key=self.sort_key),
-#                         self.batch_size, self.batch_size_fn)
-#                     for b in random_shuffler(list(p_batch)):
-#                         yield b
-#
-#             self.batches = pool(self.data(), self.random_shuffler)
-#
-#         else:
-#             self.batches = []
-#             for b in data.batch(self.data(), self.batch_size,
-#                                 self.batch_size_fn):
-#                 self.batches.append(sorted(b, key=self.sort_key))
-#
-#
-# def rebatch(pad_idx, batch):
-#     "Fix order in torchtext to match ours"
-#     src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
-#     return Batch(src, trg, pad_idx)
-#
-# if True:
-#     pad_idx = TGT.vocab.stoi["<blank>"]
-#     model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
-#     model.cuda()
-#     criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
-#     criterion.cuda()
-#     BATCH_SIZE = 12000
-#     train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
-#                             repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
-#                             batch_size_fn=batch_size_fn, train=True)
-#     valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
-#                             repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
-#                             batch_size_fn=batch_size_fn, train=False)
-#     model_par = nn.DataParallel(model, device_ids=devices)
-#
-# if True:
-#     model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
-#             torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
-#     for epoch in range(10):
-#         model_par.train()
-#         run_epoch((rebatch(pad_idx, b) for b in train_iter),
-#                   model_par,
-#                   MultiGPULossCompute(model.generator, criterion,
-#                                       devices=devices, opt=model_opt))
-#         model_par.eval()
-#         loss = run_epoch((rebatch(pad_idx, b) for b in valid_iter),
-#                           model_par,
-#                           MultiGPULossCompute(model.generator, criterion,
-#                           devices=devices, opt=None))
-#         print(loss)
-# else:
-#     model = torch.load("iwslt.pt")
+        else:
+            model = torch.load(trained_model_name).cuda()
+            model.eval()
+            dataloader = DataLoader_token_kg(filename, ents, chunk_len, device)
+            word_list = words.replace('\n', ' ').replace('\t', ' ').split(' ')
+            word_list = [i for i in word_list if (len(str(i))) != 0]
+            src = Variable(dataloader.vocabularyLoader.token_tensor(word_list).unsqueeze(0))
+            src_mask = Variable((src != 0).unsqueeze(-2))
+            ent = Variable(torch.Tensor([24]*len(word_list)).long()).to(device)
+            ents_list = []
+            for i in range(len(dataloader.kg)):
+                if words.find(" " + dataloader.kg[i] + " ") != -1:
+                    ents_list.append(dataloader.kg[i])
+            for i in range(len(ents_list)):
+                key = ents_list[i].strip().split()
+                if word_list.index(key[0]) >= 0:
+                    ent[word_list.index(key[0])] = dataloader.kg.index(" ".join(key))
+            ent = ent.unsqueeze(0)
+
+            ent_mask = None
+
+            output_embed_list = beam_search_decode_kg(model, src, src_mask, ent, ent_mask, max_len=predict_length)
+            for j in range(len(output_embed_list)):
+                output_embed = output_embed_list[j][1][0].cpu().numpy()
+                result = []
+                for i in output_embed:
+                    result.append(dataloader.vocabularyLoader.index2token[i])
+                result = result[1:]
+                result = " ".join(result)
+                print(result)
